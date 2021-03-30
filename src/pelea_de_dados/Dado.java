6bdf6a40cc510a063334d63dbcadf8d1d8964fcb
Alexander Pichard <39698441+alexanderpichard@users.noreply.github.com>
2021-03-30 10:43:50 +0200

    clase dado

--- src/pelea_de_dados/Dado.java
+++ src/pelea_de_dados/Dado.java
@@ -1,23 +1,84 @@
 package pelea_de_dados;
 
 public class Dado {
+
+	private int num;
+	private String cara;
 	
-	  //Atributos
-	   private int dado=(int)(Math.random()*(6-1+1)+1);
-	   //Constructor
-	   public Dado(){
-	       
-	   }
-	   //sets y gets
-	   public int getDado(){
-	       return dado;
-	   }
-	   public void setDado(int dado){
-	       this.dado=dado;
-	   }
-	   //Métodos necesarios para la ejecución
-	   public void lanzarDado(){
-	       System.out.println("El valor del dado es "+dado);
-	   }
+	public Dado() {
+		
+		num = (int) (Math.random()*6)+1;
+		
+		switch (num) {
+		
+		case 1:
+			cara = "---------"
+				+  "-       -"
+				+  "-   *   -"
+				+  "-       -"
+				+  "---------";
+			break;
+		case 2:
+			cara = "---------"
+					+  "-       -"
+					+  "- *   * -"
+					+  "-       -"
+					+  "---------";
+			break;
+		case 3:
+			cara = "---------"
+					+  "-   *   -"
+					+  "-       -"
+					+  "- *   * -"
+					+  "---------";
+			break;
+		case 4:
+			cara = "---------"
+					+  "- *   * -"
+					+  "-       -"
+					+  "- *   * -"
+					+  "---------";
+			break;
+		case 5:
+			cara = "---------"
+					+  "- *   * -"
+					+  "-   *   -"
+					+  "- *   * -"
+					+  "---------";
+			break;
+		case 6:
+			cara = "---------"
+					+  "- *   * -"
+					+  "- *   * -"
+					+  "- *   * -"
+					+  "---------";
+			break;
+		
+		}
+		
+	}
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getCara() {
+		return cara;
+	}
+
+	public void setCara(String cara) {
+		this.cara = cara;
+	}
+
+	@Override
+	public String toString() {
+		return cara;
+	}
+	
+	
 	
 }
