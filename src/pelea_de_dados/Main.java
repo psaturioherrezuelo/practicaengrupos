b4beb2312e768bd05d4ec4f0edc0e7a20827719c
Pablo <Pablo@DESKTOP-DNHR1A7>
2021-03-31 08:43:14 +0200

    Arreglada la visibilidad de los dados

--- src/pelea_de_dados/Main.java
+++ src/pelea_de_dados/Main.java
@@ -3,7 +3,10 @@ package pelea_de_dados;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		/*Prueba de los dados*/
+		Dados dado = new Dados();
+		System.out.println(dado);
 
 	}
 
